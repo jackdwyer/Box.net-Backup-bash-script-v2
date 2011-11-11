ee8aa01355651cd0806059ab4749607783ffee52
Jack Dwyer <jackjack.dwyer@gmail.com>
2011-11-11 16:30:16 +0000

    Working enough for my case, may look back and make it more robust. but currently works for my needs.

--- backup.py
+++ backup.py
@@ -1,9 +1,18 @@
 import os,shutil
+from datetime import datetime
 
-orig = 'original'
-backup = 'backup'
+orig = '/home/jack/web_dev/static/location/images/original'
+backup = '/home/jack/box.net/jack_servesbeer/images'
 
 diff = set(os.listdir(orig)).difference(set(os.listdir(backup)))
 for file in diff:
-	shutil.copy2('%s/%s' % (orig, file), '%s/%s' % (backup, file))
+	shutil.copyfile('%s/%s' % (orig, file), '%s/%s' % (backup, file))
+
+dbDump = '/home/jack/web_dev/dbDUMP'
+dbBackup = '/home/jack/box.net/jack_servesbeer/database'
+dbBackupName = "dbDump " + datetime.now().__str__() + ".sql"
+dbDumpName = 'sqlDump.sql'
+
+shutil.copyfile(('%s/%s' % (dbDump, dbDumpName)), ('%s/%s' % (dbBackup, dbBackupName)))
+
 
--- boxBackup.sh
+++ boxBackup.sh
@@ -5,4 +5,35 @@
 #backup script using web dav mounted box.net drive
 #--------------------
 
+#global variables
+orig='//home/jack/web_dev/static/location/images/original'
+backupImages='/home/jack/box.net/jack_servesbeer/images'
+#They are not used here, but should be passed ot python script
+
+#Database shit
+password=a
+user=root
+dbDump=("/home/jack/web_dev/dbDUMP/sqlDump.sql")
+dbDestination=("/home/jack/box.net/mysqlbackup")
+dbDumpCommand="mysqldump -u ${user} -p${password} --all-databases" 
+
+#first check webdav mount of my box.net is up
+grep -q box.net /etc/mtab
+if [ $? -eq 1 ]; then
+	mount ~/box.net
+fi
+
+#now do db dump
+$dbDumpCommand > $dbDump
+
+#run the image backup
+#TODO pass params to python script of directory locations
+
+
 python backup.py
+
+
+
+
+
+
